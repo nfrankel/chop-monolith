f0eb1acec48729803047e6c34ae084194efe449e
Nicolas Fränkel <nicolas@frankel.ch>
2022-04-08 18:40:35 +0200

    Initial commit

---
+++ chop-shop/src/main/kotlin/Checkout.kt
@@ -0,0 +1,29 @@
+package ch.frankel.chopshop
+
+import org.springframework.web.reactive.function.server.*
+
+@Suppress("UNUSED")
+class CheckoutView(val cart: Cart, val total: Double)
+
+class CheckoutHandler(private val catalog: Catalog) {
+    suspend fun displayPage(@Suppress("UNUSED_PARAMETER") req: ServerRequest): ServerResponse {
+        val cart = req.cart()
+        val total = price(cart)
+        val view = CheckoutView(cart, total)
+        return ServerResponse.ok().renderAndAwait("checkout", mapOf("checkout" to view))
+    }
+
+    suspend fun removeRow(req: ServerRequest): ServerResponse {
+        val cart = req.cart()
+        val productId = req.pathVariable("productId")
+        val product = catalog.findById(productId.toLong())
+        product?.let { cart.remove(product) }
+        return ServerResponse.ok().headers { it.set("Location", "/checkout") }.buildAndAwait()
+    }
+}
+
+fun checkoutRoutes(catalog: Catalog) = coRouter {
+    val handler = CheckoutHandler(catalog)
+    GET("/checkout", handler::displayPage)
+    DELETE("/checkout/remove/{productId}", handler::removeRow)
+}
