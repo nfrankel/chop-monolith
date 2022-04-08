67d557d253660c6289c793fd73197045120cf099
Nicolas Fränkel <nicolas@frankel.ch>
2022-04-08 18:40:35 +0200

    Initial commit

---
+++ chop-shop/src/main/kotlin/Application.kt
@@ -0,0 +1,22 @@
+package ch.frankel.chopshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.support.beans
+
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args) {
+        addInitializers(
+            beans {
+                bean { productRoutes(ref()) }
+                bean { homeRoute(ref()) }
+                bean { cartRoutes(ref()) }
+                bean { checkoutRoutes(ref()) }
+            }
+        )
+    }
+}
