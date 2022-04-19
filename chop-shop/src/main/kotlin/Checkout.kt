34953cb18cc5f44959455e69ee7c2cd263e69a04
Nicolas Fränkel <nicolas@frankel.ch>
2022-04-19 17:28:06 +0200

    Migrate to a full-fledged client-server API
      Add a REST API for the front-end

--- chop-shop/src/main/kotlin/Checkout.kt
+++ chop-shop/src/main/kotlin/Checkout.kt
@@ -3,27 +3,32 @@ package ch.frankel.chopshop
 import org.springframework.web.reactive.function.server.*
 
 @Suppress("UNUSED")
-class CheckoutView(val cart: Cart, val total: OriginPrice)
+class CheckoutView(private val cart: Cart, val total: OriginPrice) {
+    val lines: List<Pair<Product, Int>>
+        get() = cart.content.entries.map { it.toPair() }
+}
+
+internal fun Cart.toCheckout() = CheckoutView(this, price(this))
 
 class CheckoutHandler(private val catalog: Catalog) {
-    suspend fun displayPage(@Suppress("UNUSED_PARAMETER") req: ServerRequest): ServerResponse {
-        val cart = req.cart()
-        val total = price(cart)
-        val view = CheckoutView(cart, total)
-        return ServerResponse.ok().renderAndAwait("checkout", mapOf("checkout" to view))
-    }
+    suspend fun displayPage(@Suppress("UNUSED_PARAMETER") req: ServerRequest) =
+        ServerResponse.ok().renderAndAwait("checkout")
+
+    suspend fun fetchCheckout(@Suppress("UNUSED_PARAMETER") req: ServerRequest) =
+        ServerResponse.ok().bodyValueAndAwait(req.cart().toCheckout())
 
     suspend fun removeRow(req: ServerRequest): ServerResponse {
         val cart = req.cart()
         val productId = req.pathVariable("productId")
         val product = catalog.findById(productId.toLong())
         product?.let { cart.remove(product) }
-        return ServerResponse.ok().headers { it.set("Location", "/checkout") }.buildAndAwait()
+        return ServerResponse.ok().bodyValueAndAwait(req.cart().toCheckout())
     }
 }
 
 fun checkoutRoutes(catalog: Catalog) = coRouter {
     val handler = CheckoutHandler(catalog)
     GET("/checkout", handler::displayPage)
+    GET("/checkout/c", handler::fetchCheckout)
     DELETE("/checkout/remove/{productId}", handler::removeRow)
 }
