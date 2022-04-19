859463b361c3f51ca7a7e4eb15bbc8f69a6a70a7
Nicolas Fränkel <nicolas@frankel.ch>
2022-04-19 17:28:06 +0200

    Migrate to a full-fledged client-server API
      Add a REST API for the front-end

--- chop-shop/src/main/resources/static/script/checkout.js
+++ chop-shop/src/main/resources/static/script/checkout.js
@@ -1,10 +1,41 @@
 (function () {
+    const fetchCheckout = async () => {
+        const response = await fetch('/checkout/c')
+        displayCheckout(await response.json())
+        attachEventHandlers()
+    }
+    const displayCheckout = checkout => {
+        const tbody = document.getElementById('checkout')
+        while (tbody.firstChild) {
+            tbody.removeChild(tbody.firstChild);
+        }
+        checkout.lines.forEach(displayCheckoutLine)
+        let total = new Intl.NumberFormat('en', { style: 'currency', currency: 'USD' }).format(checkout.total)
+        document.getElementById('total').innerHTML = total
+    }
+    const displayCheckoutLine = line => {
+        const fragment = document.querySelector('#line').content.cloneNode(true)
+        const image = fragment.querySelector('img')
+        image.src = `/image/${line.first.id}.jpg`
+        const nameCell = fragment.querySelectorAll('td')[1]
+        nameCell.innerHTML = line.first.name
+        const priceCell = fragment.querySelectorAll('td')[2]
+        let price = new Intl.NumberFormat('en', { style: 'currency', currency: 'USD' }).format(line.first.price)
+        priceCell.innerHTML = price
+        const qtyCell = fragment.querySelectorAll('td')[3]
+        qtyCell.innerHTML = line.second
+        const btn = fragment.querySelector('a')
+        btn.dataset.productId = line.first.id
+        const icon = fragment.querySelector('i')
+        icon.dataset.productId = line.first.id
+        document.getElementById('checkout').appendChild(fragment)
+    }
     const removeFromCheckout = async event => {
         const response = await fetch(
             `/checkout/remove/${event.target.dataset.productId}`,
             { method: 'DELETE' }
         )
-        location.assign(await response.headers.get('location'))
+        displayCheckout(await response.json())
     }
     const attachEventHandlerToRemoveButton = button => {
         button.onclick = removeFromCheckout
@@ -13,6 +44,6 @@
         document.querySelectorAll('.remove-from-checkout').forEach(attachEventHandlerToRemoveButton)
     }
     window.onload = () => {
-        attachEventHandlers()
+        fetchCheckout()
     }
 })()
--- chop-shop/src/main/resources/static/script/home.js
+++ chop-shop/src/main/resources/static/script/home.js
@@ -1,4 +1,39 @@
 (function () {
+    const fetchCatalog = async event => {
+        const response = await fetch('/catalog')
+        displayProducts(await response.json())
+        attachEventHandlers()
+        displayCart()
+    }
+    const displayProducts = products => {
+        products.forEach(displayProduct)
+    }
+    const displayProduct = product => {
+        const fragment = document.querySelector('#product').content.cloneNode(true)
+        const card = fragment.querySelector('.card')
+        if (!product.hero) {
+            const ribbon = card.querySelector('.ribbon')
+            ribbon.remove()
+        }
+        const img = card.querySelector('img')
+        img.src = `/image/${product.id}.jpg`
+        const title = card.querySelector('h5')
+        title.innerText = product.name
+        const text = card.querySelector('p')
+        text.innerText = product.description
+        const a = card.querySelector('a.btn')
+        a.dataset.productId = product.id
+        const icon = card.querySelector('i.bi')
+        icon.dataset.productId = product.id
+        const badge = card.querySelector('span.badge')
+        badge.innerText = new Intl.NumberFormat('en', { style: 'currency', currency: 'USD' }).format(product.price)
+        badge.dataset.productId = product.id
+        document.getElementById('products').appendChild(fragment)
+    }
+    const displayCart = async event => {
+        const response = await fetch('/cart/quantity')
+        updateQuantity(await response.json())
+    }
     const addToCart = async event => {
         const response = await fetch(
             `/cart/add/${event.target.dataset.productId}`,
@@ -20,6 +55,6 @@
         document.querySelectorAll('.add-to-cart').forEach(attachEventHandlerToProductButton)
     }
     window.onload = () => {
-        attachEventHandlers()
+        fetchCatalog()
     }
 })()
