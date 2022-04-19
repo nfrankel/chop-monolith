65f9ea89770e4309fb35637538611d9fe06dff13
Nicolas Fränkel <nicolas@frankel.ch>
2022-04-19 17:28:06 +0200

    Migrate to a full-fledged client-server API
      Add a REST API for the front-end

--- chop-shop/src/main/kotlin/Home.kt
+++ chop-shop/src/main/kotlin/Home.kt
@@ -1,21 +1,17 @@
 package ch.frankel.chopshop
 
-import org.springframework.web.reactive.function.server.ServerRequest
-import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.coRouter
-import org.springframework.web.reactive.function.server.renderAndAwait
+import org.springframework.web.reactive.function.server.*
 
 class HomeHandler(private val catalog: Catalog) {
     suspend fun displayPage(@Suppress("UNUSED_PARAMETER") req: ServerRequest) =
-        ServerResponse.ok().renderAndAwait(
-            "home", mapOf(
-                "products" to catalog.findAll(),
-                "cart" to CartQuantityView(req.cart())
-            )
-        )
+        ServerResponse.ok().renderAndAwait("home")
+
+    suspend fun fetchCatalog(@Suppress("UNUSED_PARAMETER") req: ServerRequest) =
+        ServerResponse.ok().bodyAndAwait(catalog.findAll())
 }
 
 fun homeRoute(catalog: Catalog) = coRouter {
     val handler = HomeHandler(catalog)
     GET("/", handler::displayPage)
+    GET("/catalog", handler::fetchCatalog)
 }
