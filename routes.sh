a12221c49350fb61bee9dca5bb4f413b710141f7
Nicolas Fränkel <nicolas@frankel.ch>
2022-04-20 16:35:45 +0200

    Set up the architecture on Docker Compose

---
+++ routes.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+docker run --network chop-monolith_default --rm curlimages/curl:7.81.0 -v -i http://apisix:9180/apisix/admin/routes/1 -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -d '
+{
+  "uri": "/*",
+  "plugins" : {
+    "response-rewrite": {
+      "headers": {
+        "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate",
+        "Expires": 0,
+        "Pragma": "no-cache"
+      }
+    }
+  },
+  "upstream": {
+    "type": "roundrobin",
+    "nodes": {
+      "chopshop:8080": 1
+    }
+  }
+}'
