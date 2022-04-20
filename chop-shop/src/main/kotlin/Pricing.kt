d2e8b13495753d0c7ceca87a72b8e111272727ff
Nicolas Fränkel <nicolas@frankel.ch>
2022-04-20 19:26:52 +0200

    Make pricing an HTTP route

--- chop-shop/src/main/kotlin/Pricing.kt
+++ chop-shop/src/main/kotlin/Pricing.kt
@@ -1,13 +1,29 @@
 package ch.frankel.chopshop
 
+import kotlinx.coroutines.reactor.awaitSingle
+import org.springframework.web.reactive.function.server.*
+
 data class OriginPrice(
     val price: Double,
     val origin: String = "monolith"
 )
 
-fun price(cart: Cart) = cart.content.entries
-    .fold(0.0) { current, entry ->
-        current + entry.key.price * entry.value
+private fun Double.toOriginPrice() = OriginPrice(this)
+
+fun price(checkout: CheckoutView) = checkout.lines
+    .fold(0.0) { current, line ->
+        current + line.first.price * line.second
     }.toOriginPrice()
 
-private fun Double.toOriginPrice() = OriginPrice(this)
+class PricingHandler {
+    suspend fun compute(req: ServerRequest): ServerResponse {
+        val cart = req.bodyToMono<CheckoutView>().awaitSingle()
+        val price = price(cart)
+        return ServerResponse.ok().bodyValueAndAwait(price)
+    }
+}
+
+fun pricingRoute() = coRouter {
+    val handler = PricingHandler()
+    POST("/price", handler::compute)
+}
