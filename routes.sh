15dcb949690e9a119b44b352d0be84f6cd05b810
Nicolas Fränkel <nicolas@frankel.ch>
2022-04-21 16:23:58 +0200

    Add pricing function code that is deployed on Azure

--- routes.sh
+++ routes.sh
@@ -17,3 +17,20 @@ docker run --network chop-monolith_default --rm curlimages/curl:7.81.0 -v -i htt
     }
   }
 }'
+
+source .env
+
+docker run --network chop-monolith_default --rm curlimages/curl:7.81.0 -v -i http://apisix:9180/apisix/admin/routes/2 -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -d '
+{
+  "methods": ["POST"],
+  "uris": ["/price"],
+  "plugins": {
+    "azure-functions": {
+      "function_uri": "https://chopshoppricing.azurewebsites.net/api/HttpTrigger",
+      "authorization": {
+        "apikey": "'"$AZURE_FUNCTION_KEY"'"
+      },
+      "ssl_verify": false
+    }
+  }
+}'
